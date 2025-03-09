62575ad2eb79668a920d68ccfe61a046e576a4be
DavidBrada <153826133+DavidBrada@users.noreply.github.com>
2025-03-09 16:26:29 +0100

    Final version

--- Web/index.js
+++ Web/index.js
@@ -1,1 +1,24 @@
-console.log('Happy developing ✨')
+let currentSlide = 1;
+ShowSlide(currentSlide);
+
+function ChangeSlide(){
+    if(currentSlide == 2){
+        currentSlide--;
+    }
+    else{
+        currentSlide++;
+    }
+    ShowSlide(currentSlide);
+}
+
+function ShowSlide(currentSlide){
+
+    if(currentSlide == 1){
+        document.getElementById(2).style.display = "none";
+    }
+    else if(currentSlide == 2){
+        document.getElementById(1).style.display = "none";
+    }
+
+    document.getElementById(currentSlide).style.display = "block";
+}
